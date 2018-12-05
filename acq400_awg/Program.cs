6f99a76cf8bf18055c667fc3d2a1493a48eab9cd
Peter Milne on rockall <peter.milne@d-tacq.com>
2018-12-05 07:26:10 +0000

    Add project files.

---
+++ acq400_awg/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace acq400_awg
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("acq400_awg " + string.Join(" ", args));
+        }
+    }
+}
