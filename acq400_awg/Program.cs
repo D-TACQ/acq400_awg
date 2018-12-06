a5bdf1255d135b04902cc7b65f1583e95ac3b37b
Peter Milne on rockall <peter.milne@d-tacq.com>
2018-12-06 21:58:47 +0000

    SiteClient objects "work"

--- acq400_awg/Program.cs
+++ acq400_awg/Program.cs
@@ -1,14 +1,66 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net.Sockets;
 
 namespace acq400_awg
 {
-
-    class Program
+    class SiteClient
     {
+        TcpClient sk;
+        string uut;
+        int site;
+        BinaryWriter writer;
+        BinaryReader reader;
+        string model;
+
+        string ReadString()
+        {
+            string response = "";
+            bool done = false;
+            while (!done)
+            {
+                char[] lastchar = reader.ReadChars(1);
+                if (lastchar[0] != '\n')
+                {
+                    response += lastchar[0];
+                }
+                else
+                {
+                    done = true;
+                }
+            }
+            return response;
+        }
+        public SiteClient(string _uut, int _site)
+        {
+            uut = _uut;
+            site = _site;
+            sk = new TcpClient(uut, 4220+site);
+            writer = new BinaryWriter(sk.GetStream());
+            reader = new BinaryReader(sk.GetStream());
+            model = GetKnob("MODEL");
+        }
+        public void SetKnob(string knob, string value)
+        {
+            writer.Write(knob + "=" + value + "\n");
+            Console.WriteLine(ReadString());
+        }
+        public string GetKnob(string knob)
+        {
+            writer.Write(knob + "\n");
+            string rs = ReadString();
+            Console.WriteLine(rs);
+            return rs;
+        }
+    }
+
+    class AWG_Controller
+    {
+        SiteClient s0;
+        SiteClient s1;
+
+     
         void SetMaxLen(int len)
         {
             Console.WriteLine("SetMaxLen:" + len);
@@ -28,13 +80,15 @@ namespace acq400_awg
             for (int ii = 0; ii < reps; ++ii)
                 for (int fn = 0; fn < fnames.Length; ++fn)
                 {
-                    Console.WriteLine("rep " + reps + " load " + fnames[fn]);
+                    Console.WriteLine(uut + " rep " + reps + " load " + fnames[fn]);
                 }
         }
         string uut;
-        Program(string _uut)
+        AWG_Controller(string _uut)
         {
             uut = _uut;
+            s0 = new SiteClient(uut, 0);
+            s1 = new SiteClient(uut, 1);
         }
         static void Main(string[] args)
         {
@@ -45,8 +99,8 @@ namespace acq400_awg
             } 
             else
             {
-                Program p = new Program(args[0]);
-                p.RunLoop(Convert.ToInt32(args[1]), new List<string>(args).GetRange(2, args.Length-2).ToArray());
+                AWG_Controller ctrl = new AWG_Controller(args[0]);
+                ctrl.RunLoop(Convert.ToInt32(args[1]), new List<string>(args).GetRange(2, args.Length-2).ToArray());
             }
         }
     }
